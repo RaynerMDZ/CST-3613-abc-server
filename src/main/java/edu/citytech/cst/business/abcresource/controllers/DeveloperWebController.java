4b99915c58e53a6583e50bfb413c31b2eb3696ee
Rayner Mendez <raynermendezg@gmail.com>
2021-03-17 13:48:08 -0400

    Add testing endpoints.

--- src/main/java/edu/citytech/cst/business/abcresource/controllers/DeveloperWebController.java
+++ src/main/java/edu/citytech/cst/business/abcresource/controllers/DeveloperWebController.java
@@ -1,15 +1,34 @@
 package edu.citytech.cst.business.abcresource.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/developer")
 public class DeveloperWebController {
 
     @GetMapping
-    public String getDeveloperInfo() {
-        return "Rayner Mendez";
+    public ResponseEntity<String> getDeveloperInfo() {
+        return new ResponseEntity<>("Rayner Mendez" ,HttpStatus.OK);
     }
+
+    @GetMapping("/more-info")
+    public ResponseEntity<Map<String, String>> getDeveloperMoreInfo() {
+        Map<String, String> info = new HashMap<>();
+        info.put("name", "Rayner Mendez");
+        info.put("email", "rayner.mendez@mail.citytech.cuny.edu");
+        info.put("title", "developer");
+
+        return new ResponseEntity<>(info ,HttpStatus.OK);
+    }
+
+    @PostMapping("{name}")
+    public ResponseEntity<String> greeting(@PathVariable(name = "name") String name) {
+        return new ResponseEntity<>("Hello " + name + ", How are you?", HttpStatus.OK);
+    }
+
 }
